cae4f0b3f63ec2c81c067910cb2626551648cb25
Adjay Thakoerdien <a.thakoerdien@gmail.com>
2021-02-28 14:43:27 +0100

    V0.5

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,3 +8,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    function = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    text = models.TextField()
+    image = models.ImageField(upload_to="portfolio/images/")
+
+    def __str__(self):
+        return self.name
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Testimonial
+from blog.models import Blogpost
 
 # Create your views here.
 def home(request):
+    testimonials = Testimonial.objects.all()
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects':projects})
+    blog1 = Blogpost.objects.order_by('-date')[0]
+    blog2 = Blogpost.objects.order_by('-date')[1]
+    blog3 = Blogpost.objects.order_by('-date')[2]
+    blog4 = Blogpost.objects.order_by('-date')[3]
+    lijst = ['Python', 'Django', 'Bootstrap', 'SQLite', 'HTML/CSS']
+
+    return render(request, 'portfolio/home.html',
+    {'testimonials':testimonials,'projects':projects,'blog1':blog1,'blog2':blog2,'blog3':blog3,'blog4':blog4,'lijst':lijst})
