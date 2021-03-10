f6cd9e771f54d6acdd8893dc379fe0cdde644eab
Adjay Thakoerdien <a.thakoerdien@gmail.com>
2021-03-10 14:01:53 +0100

    V0.6.5

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
+    path('todo/', include('todo.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
