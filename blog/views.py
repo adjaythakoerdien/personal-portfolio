d2fcc9ad75e32600100a067e2648a6e956451569
Adjay Thakoerdien <a.thakoerdien@gmail.com>
2021-08-25 15:50:08 +0200

    V0.8.111

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blogpost
+from portfolio.models import Intro
 
 def all_blogs(request):
+    intro = Intro.objects.all()
     blogs = Blogpost.objects.order_by('-date')[:5]
     laatsteblog = Blogpost.objects.order_by('-date')[0]
-    return render(request, 'blog/all_blogs.html', {'blogs':blogs,'laatsteblog':laatsteblog, 'pagina': 'blog'})
+
+    return render(request, 'blog/all_blogs.html', {'intro':intro,
+        'blogs':blogs,'laatsteblog':laatsteblog, 'pagina': 'blog'})
+
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blogpost, pk=blog_id)
