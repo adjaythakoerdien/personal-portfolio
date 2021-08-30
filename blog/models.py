d7bffe7b0e44d4c4c041cf47041ef84f4a7c9bdd
Adjay Thakoerdien <a.thakoerdien@gmail.com>
2021-08-30 07:11:03 +0200

    V0.8.5.2

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ class Blogpost(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
+    image = models.ImageField(upload_to="portfolio/images/", default='portfolio/images/project-figure-main1.png')
+    image_2 = models.ImageField(upload_to="portfolio/images/", blank=True,
+                                default='portfolio/images/project-figure-main1.png')
 
     def __str__(self):
         return self.title
